52ef14734640b5674304da717fd1c4021e9bb78f
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-12 10:57:29 +0200

    register.js () ergänzt
    dadurch user und pw in console angezeigt & bleibt

--- js/register.js
+++ js/register.js
@@ -3,7 +3,7 @@ console.log("hello from register js!");
 document
 .getElementById("registerForm")
 .addEventListener("submit", async (e) => {
-    e.preventDefault;
+    e.preventDefault();
 
     let username = document.querySelector("#username").value;
     console.log(username);
