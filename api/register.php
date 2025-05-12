4251af29e519f68edbfd76290101bdf3d3d46a0f
wera-burkhalter <wera.burkhalter@stud.fhgr.ch>
2025-05-12 12:16:38 +0200

    Daten einfügen in DB

--- api/register.php
+++ api/register.php
@@ -1,10 +1,19 @@
 <?php
 
+require_once('../system/config.php');
+
 header('Content-Type: text/plain; charset=UTF-8');
 
 $username = $_POST['username'] ?? '';
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
+
+$insert = $pdo->prepare("INSERT INTO benutzer (username, email, password) VALUES (:username, :email, :password)");
+$insert->execute([
+    ':username' => $username,
+    ':email' => $email,
+    ':password' => $password
+]);
 
 echo "Username: {$username}\n";
 echo "E-Mail: {$email}\n";
