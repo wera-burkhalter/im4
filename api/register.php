9dc244ae486ae1c7870ba829410212a9eb366f59
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-12 13:46:22 +0200

    register.php verlinkung zu config.php geupdated

--- api/register.php
+++ api/register.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once('../system/config.php');
+require_once(__DIR__ . '/../config.php');
 
 header('Content-Type: text/plain; charset=UTF-8');
 
