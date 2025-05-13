2839bd7915365a6ef6b933b5fd140880c7ee133c
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 10:12:56 +0200

    update login.js farmData

--- js/login.js
+++ js/login.js
@@ -11,4 +11,26 @@ document
 console.log("loginInfo ist:", loginInfo);
 console.log("password ist:", password);
 
+
+
+    //Formulardaten vorbereiten
+    const formData = new FormData();
+    formData.append("loginInfo", phone);
+    formData.append("password", password);
+
+    // Senden
+    try {
+        const res = await fetch("api/login.php", {
+            method: "POST",
+            body: formData,
+        });
+
+        const data = await res.text(); // falls du auf JSON umstellst: `await res.json()`
+        
+        console.log("Antwort vom Server:\n" + data);
+        alert(data); // einfache Ausgabe im Browser
+    } catch (error) {
+        console.error("Fehler beim Senden der Anfrage:", error);
+        alert("Ein Fehler ist aufgetreten.");
+    }
 });
