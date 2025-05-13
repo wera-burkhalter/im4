78ee7e8cac5fab8f8b01260f2fd1f2e926503969
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 10:28:40 +0200

    Update login.php

--- api/login.php
+++ api/login.php
@@ -10,3 +10,16 @@ $password = $_POST['password'] ?? '';
 
 // Validierung der Eingabefelder
 echo "Habe folgende Daten erhalten, phone: $phone\n, Passwort: $password\n";
+
+// Prüfen, ob Telefonnummer bereits existiert
+$stmt = $pdo->prepare("SELECT * FROM benutzer WHERE phone = :phone");
+$stmt->execute([':phone' => $phone]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$user) {
+    echo "Telefon gefunden, jetzt Passwort prüfen";
+
+} else {
+    echo "Telefon nicht korrekt";
+    
+}
