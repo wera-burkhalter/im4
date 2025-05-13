599262e024e910dab9c7abc0dca95d0fbf2555c3
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 09:56:35 +0200

    login.js geupdated

--- js/login.js
+++ js/login.js
@@ -1,1 +1,14 @@
 console.log("login.js loaded");
+
+document
+.getElementById("registerForm")
+.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const loginInfo = document.querySelector("#userPhone").value.trim();
+    const password = document.querySelector("#password").value;
+
+console.log("loginInfo ist:", loginInfo);
+console.log("password ist:", password);
+
+});
