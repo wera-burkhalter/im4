736ce4b533102d70257192debfb09d7378a0e350
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 10:19:04 +0200

    Update login.php

--- api/login.php
+++ api/login.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/../config.php');
 header('Content-Type: text/plain; charset=UTF-8');
 
 // Formulardaten
-$loginInfo = $_POST['phone'] ?? '';
+$phone = $_POST['phone'] ?? '';
 $password = $_POST['password'] ?? '';
 
 // Validierung der Eingabefelder
