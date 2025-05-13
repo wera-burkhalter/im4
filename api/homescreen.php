66cabcb04514d46f6952c6a17f48377ac43b2733
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 11:30:29 +0200

    homescreen.php
    name & telefonnummer ergänzt

--- api/homescreen.php
+++ api/homescreen.php
@@ -3,7 +3,10 @@
 session_start();
 
 if (isset($_SESSION['user_id'])) {
-    echo "User eingeloggt";
+    echo "User eingeloggt, ID:" . $_SESSION
+    ['user_id'] . ", Name:" . $_SESSION['firstName'];
+    ['username'] . "Handy-Nr.:" . $_SESSION['phone'];
+    
 } else {
     echo "User nicht eingeloggt";
 }
