ca3a9b36e4094d47c16d206d52cddbd0675a3f13
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 10:44:31 +0200

    fehleranzeige ergänzt

--- api/login.php
+++ api/login.php
@@ -1,4 +1,8 @@
 <?php
+// Fehleranzeige aktivieren (für Entwicklung)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once(__DIR__ . '/../config.php');
 
