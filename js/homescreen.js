7ec54bb1f586c94c290b4a2a8df924ae2d10f873
NicoleRau <nicole.rau@stud.fhgr.ch>
2025-05-13 11:45:19 +0200

    homescreen.js aufgebaut

--- js/homescreen.js
+++ js/homescreen.js
@@ -1,1 +1,9 @@
 console.log("homescreen.js loaded");
+
+//Infos aus dem PHP holen
+
+fetch("api/homescreen.php")
+    .then ((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    });
