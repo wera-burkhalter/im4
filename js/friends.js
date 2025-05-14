d0f3e093dc18ea7b57e90e1f66f661b1082265b1
wera-burkhalter <wera.burkhalter@stud.fhgr.ch>
2025-05-14 08:19:26 +0200

    friends.js: session daten mit Profilbild holen

--- js/friends.js
+++ js/friends.js
@@ -6,6 +6,23 @@ const message = document.getElementById("addFriendMessage");
 
 let currentFriendId = null;
 
+// ========================
+// 1. SESSIONDATEN HOLEN
+// ========================
+fetch("api/protected.php")
+  .then(res => res.json())
+  .then(data => {
+    if (data.status === "error") {
+      window.location.href = "login.html";
+    } else {
+      currentUserId = data.user_id;
+      if (data.profilbild) {
+        profilbild.src = data.profilbild;
+      }
+      loadEvents();
+    }
+  });
+
 // =============================
 // 2. FREUNDE LADEN
 // =============================
