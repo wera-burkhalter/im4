55771ee30977744d916e234b96db911f87de81f4
wera-burkhalter <wera.burkhalter@stud.fhgr.ch>
2025-05-14 08:26:35 +0200

    homescreen.js akt.

---
+++ js/homescreen.js
@@ -0,0 +1,16 @@
+// ========================
+// 1. SESSIONDATEN HOLEN
+// ========================
+fetch("api/protected.php")
+  .then(res => res.json())
+.then(data => {
+  if (data.status === "error") {
+    window.location.href = "login.html";
+  } else {
+    currentUserId = data.user_id;
+    if (data.profilbild) {
+      profilbild.src = data.profilbild;
+    }
+    ladeFreunde(); // ✅ Hier war der Fehler
+  }
+});
